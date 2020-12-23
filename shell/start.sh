03a1f4d2fbf74dbc5092c9dece8c2708c2ae4f8b
yaoyang <kiki@smarty.so>
2020-12-23 17:41:47 +0800

    debug 在 arm 环境镜像启动时报错的问题

--- shell/start.sh
+++ shell/start.sh
@@ -14,9 +14,9 @@ service beanstalkd start
 service supervisor start
 service mongodb start
 
-mysql -e "create database \`default\`"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password'"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'password'"
+mysql -e "create database \`default\`;\
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password';\
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'password'"
 
 date > /tmp/php_exception.log
 date > /tmp/php_notice.log
